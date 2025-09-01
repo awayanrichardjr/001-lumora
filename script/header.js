3777a25aae86483705b62b918d656550673d3c5f
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-01 19:23:56 +0800

    cleaning header js

--- script/header.js
+++ script/header.js
@@ -1,14 +1,13 @@
+const headerrr = document.getElementsByClassName("header")[0];
+
 function room00() {
-  document.getElementsByClassName("header")[0].style.backgroundImage =
-    "url('./img/rooms/room00.webp')";
+  headerrr.style.backgroundImage = "url('./img/rooms/room00.webp')";
 }
 function room01() {
-  document.getElementsByClassName("header")[0].style.backgroundImage =
-    "url('./img/rooms/room01.webp')";
+  headerrr.style.backgroundImage = "url('./img/rooms/room01.webp')";
 }
 function room02() {
-  document.getElementsByClassName("header")[0].style.backgroundImage =
-    "url('./img/rooms/room02.jpeg')";
+  headerrr.style.backgroundImage = "url('./img/rooms/room02.jpeg')";
 }
 
 function spanValue() {
