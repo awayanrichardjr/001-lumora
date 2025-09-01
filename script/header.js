16e9a76fadb9ab094e0d129919b4efb5db78ad6a
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-01 16:16:11 +0800

    adding script to span value

--- script/header.js
+++ script/header.js
@@ -10,3 +10,12 @@ function room02() {
   document.getElementsByClassName("header")[0].style.backgroundImage =
     "url('./img/rooms/room02.jpeg')";
 }
+
+function spanValue() {
+  let spanValue = document.getElementById("chevron_left").innerHTML;
+  if (spanValue === "chevron_left") {
+    document.getElementById("chevron_left").innerHTML = "chevron_right";
+  } else {
+    document.getElementById("chevron_left").innerHTML = "chevron_left";
+  }
+}
