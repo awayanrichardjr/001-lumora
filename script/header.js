9898b13e9f216e362d9a3acf204f9df592154c7a
Richard Awayan <awayanrichardjr@gmail.com>
2025-09-02 13:16:32 +0800

    updating sidebar content width

--- script/header.js
+++ script/header.js
@@ -13,16 +13,38 @@ function room02() {
 function spanValue() {
   const sidebarContent = document.getElementById("sidebar-content");
   const chevronLeft = document.getElementById("chevron_left");
+  const sidebarOpenConfig = {
+    chevronLeft: {
+      innerHTML: "chevron_right",
+    },
+    sidebarContent: {
+      width: "350px",
+      opacity: "1",
+      paddingInlineStart: "1rem",
+      paddingInlineEnd: "4rem",
+      transition: "0.5s",
+    },
+    sidebarContent425Media: {
+      width: "300px",
+      opacity: "1",
+      paddingInlineStart: "1rem",
+      paddingInlineEnd: "4rem",
+      transition: "0.5s",
+    },
+  };
 
   let spanValue = chevronLeft.innerHTML;
   if (spanValue === "chevron_left") {
-    chevronLeft.innerHTML = "chevron_right";
-
-    sidebarContent.style.width = "350px";
-    sidebarContent.style.opacity = "1";
-    sidebarContent.style.paddingInlineStart = "1rem";
-    sidebarContent.style.paddingInlineEnd = "4rem";
-    sidebarContent.style.transition = "0.5s";
+    if (window.matchMedia("(max-width: 426px)").matches) {
+      chevronLeft.innerHTML = sidebarOpenConfig.chevronLeft.innerHTML;
+      Object.assign(
+        sidebarContent.style,
+        sidebarOpenConfig.sidebarContent425Media
+      );
+    } else {
+      chevronLeft.innerHTML = sidebarOpenConfig.chevronLeft.innerHTML;
+      Object.assign(sidebarContent.style, sidebarOpenConfig.sidebarContent);
+    }
   } else {
     chevronLeft.innerHTML = "chevron_left";
     sidebarContent.style.width = "0";
